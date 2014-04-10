7c553184271248d3521e06961e4c9cc8e1e4f7d7
Stephen Gregory <stephen.gregory@oracle.com>
2014-04-10 10:20:12 -0400

    Updated code to work better and keep going

--- src/Compendium/Beaneater.hs
+++ src/Compendium/Beaneater.hs
@@ -9,38 +9,35 @@ import qualified Data.ByteString.Char8 as B
 
 eatBeans :: BeanstalkServer -> IO ()
 eatBeans bs = do 
-				watchAllTubes bs
-				eatNBeans bs 90
-
-
-
-
+            watchAllTubes bs
+            eatNBeans bs 10000
+            eatBeans bs
 
 eatNBeans :: BeanstalkServer -> Int  -> IO ()
 eatNBeans bs times  = if times <= 0 then return ()  else (eatBean bs 60) >>
-														(eatNBeans bs (times -1))
+                                                (eatNBeans bs (times -1))
 
 ignoreBeanstalkError :: BeanstalkException -> IO ()
-ignoreBeanstalkError ex = return ()
+ignoreBeanstalkError err =  putStr ("Error Caught " ++ (show err) ++" \n") 
 
+
+                            
 -- Takes a beanstalk and int
 eatBean :: BeanstalkServer -> Int -> IO ()
-eatBean bs timeout = handle ignoreBeanstalkError   (eatBean'  bs timeout)
-
-eatBean' :: BeanstalkServer -> Int -> IO()
-eatBean' bs timeout = do
+eatBean bs timeout = catch  (eatBean'  bs timeout) ignoreBeanstalkError
+            where 
+                eatBean' bs timeout = do 
 			job <- reserveJobWithTimeout bs timeout 
+                        --putStr ("Eating Job " ++ show (job_id job) ++ "\n")
 			deleteJob bs (job_id job)
 
 
-watchTubes :: BeanstalkServer -> [B.ByteString] -> IO ()
-watchTubes bs (x:xs) = watchTube bs x >> watchTubes bs xs
-watchTubes bs []   = return ()
-
 
 watchAllTubes :: BeanstalkServer -> IO ()
-watchAllTubes bs = listTubes bs >>= (watchTubes bs) >> return ()
-
+watchAllTubes bs = listTubes bs >>= watchTubes bs  >> return ()
+                   where 
+                        watchTubes bs (x:xs) = watchTube bs x  >> watchTubes bs xs
+                        watchTubes _ []   = return ()
 
 
 
--- src/Main.hs
+++ src/Main.hs
@@ -2,10 +2,18 @@ module Main where
 
 import Compendium.Beaneater
 import Network.Beanstalk
+import Control.Concurrent
 import System.Environment
 
-main = do 
-		argv <- getArgs
-		let host : port : xs = argv
+
+
+runEater  host port =  do
     		bs <- connectBeanstalk host port
     		eatBeans bs
+
+main = do 
+        argv <- getArgs
+        let host : port : xs = argv
+        mapM (\_  -> (forkIO (runEater host port))) [1..100] 
+        runEater host port
+
